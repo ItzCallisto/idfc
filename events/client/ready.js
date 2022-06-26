5e1e617a02fefa8b371855c6a87dfc994e5caa90
ItzCallisto <102191146+ItzCallisto@users.noreply.github.com>
2022-06-26 13:44:36 +0100

    hello bozo

---
+++ events/client/ready.js
@@ -0,0 +1,48 @@
+//here the event starts
+let config = require(`${process.cwd()}/botconfig/config.json`)
+const Discord = require("discord.js")
+const moment = require("moment")
+const { nFormatter } = require(`${process.cwd()}/handlers/functions`)
+module.exports = client => {
+  //SETTING ALL GUILD DATA FOR THE DJ ONLY COMMANDS for the DEFAULT
+  //client.guilds.cache.forEach(guild=>client.settings.set(guild.id, ["autoplay", "clearqueue", "forward", "loop", "jump", "loopqueue", "loopsong", "move", "pause", "resume", "removetrack", "removedupe", "restart", "rewind", "seek", "shuffle", "skip", "stop", "volume"], "djonlycmds"))
+  try{
+    try{
+      const stringlength = 69;
+      console.log("\n")
+      console.log(`     ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓`.bold.brightGreen)
+      console.log(`     ┃ `.bold.brightGreen + " ".repeat(-1+stringlength-` ┃ `.length)+ "┃".bold.brightGreen)
+      console.log(`     ┃ `.bold.brightGreen + `Discord Bot is online!`.bold.brightGreen + " ".repeat(-1+stringlength-` ┃ `.length-`Discord Bot is online!`.length)+ "┃".bold.brightGreen)
+      console.log(`     ┃ `.bold.brightGreen + ` /--/ ${client.user.tag} /--/ `.bold.brightGreen+ " ".repeat(-1+stringlength-` ┃ `.length-` /--/ ${client.user.tag} /--/ `.length)+ "┃".bold.brightGreen)
+      console.log(`     ┃ `.bold.brightGreen + " ".repeat(-1+stringlength-` ┃ `.length)+ "┃".bold.brightGreen)
+      console.log(`     ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛`.bold.brightGreen)
+    } catch { /* */ }
+
+   
+    
+    status(client);
+    //loop through the status per each 10 minutes
+    
+  
+  } catch (e){
+    console.log(String(e.stack).grey.bgRed)
+  }
+}
+var state = false;
+function status(client){
+  config = require(`${process.cwd()}/botconfig/config.json`)
+  if(!state){
+    client.user.setActivity("!help", {type: "STREAMING"})
+  }
+  
+  
+}
+/**
+  * @INFO
+  * Bot Coded by Tomato#6966 | https://discord.gg/milrato
+  * @INFO
+  * Work for Milrato Development | https://milrato.eu
+  * @INFO
+  * Please mention him / Milrato Development, when using this Code!
+  * @INFO
+*/
